31efd6fc03b0d7f55a74af00b3c1abb1185e3ac8
gomzaemon111 <goki14914@icloud.com>
2019-05-21 22:14:40 +0900

    add grammers/atom.ex

--- grammers/atom.ex
+++ grammers/atom.ex
@@ -1,3 +1,6 @@
 IO.puts :foo # => foo
 IO.puts :foo == :hoge # => false
 # ruby のシンボル型と同じ
+# Elixirのモジュールの名前もまたアトムです。MyApp.MyModuleは、そのようなモジュールが宣言されていなくても有効なアトムです。
+
+IO.puts is_atom(MyApp.MyModule)
