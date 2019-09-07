b9dbd1ede3282bc01fe9eca4d4efa4ce241c636c
gonzaemon111 <goki14914@icloud.com>
2019-09-08 00:02:51 +0900

    add grammers/list.ex

--- grammers/list.ex
+++ grammers/list.ex
@@ -1,3 +1,14 @@
 ar = [1,2,3]
 a = Enum.at(ar, 1)
 IO.puts a
+
+list = [3.14, :pie, "Apple"]
+IO.inspect list # listの場合は、inspectメソッドでデバッグ風に標準出力する。
+
+Enum.each list, fn n ->
+  IO.puts n
+end
+# IO.puts(list)
+
+# list ++ ["Cherry"]
+# IO.puts list
